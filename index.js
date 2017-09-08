bc688058944a3f0d62394ae2e11a341f020856f0
Nattapol <nattapol.phakhakit@hotmail.com>
2017-09-08 22:33:39 +0700

    Add beer search

--- index.js
+++ index.js
@@ -1,51 +1,40 @@
-function search() {
+function search()
+{
 	var url = '';
-	var list = document.getElementById('data');
-	list.innerHTML = '';
-
+	document.getElementById('data').innerHTML = '';
 	var text = document.getElementById('text').value;
-	if (document.getElementById('name').checked) {
+	
+	if (document.getElementById('name').checked)
+	{
 		var url = 'https://api.punkapi.com/v2/beers/?beer_name=' + text;
-	} else {
+	}
+	else
+	{
 		var url = 'https://api.punkapi.com/v2/beers/?food=' + text;
 	}
-	fetch(url).then(function (r) {
+	
+	fetch(url).then(function (r)
+	{
 		return r.json();
-	}).then(function (data) {
-		for (var i = 0; i < data.length; i++) {
-			var div = document.createElement('div');
-			div.setAttribute('class', 'item');
-			
-			var ul = document.createElement('li');
-			ul.setAttribute('style', 'clear:both; padding:10px;');
-			var img = document.createElement('img');
-			img.setAttribute('src', data[i].image_url);
-			img.setAttribute('height', '160px');
-			img.setAttribute('width', '40px');
-			img.setAttribute('style', 'float:left');
-
-			var name = document.createElement('h4');
-			var beerName = document.createTextNode('Name : ' + data[i].name);
-			name.appendChild(beerName);
-
-			var food = document.createElement('li');
-			var beerFood = document.createTextNode('Food Pairing : ' + data[i].food_pairing);
-			food.appendChild(beerFood);
-			food.style.fontSize = "small";
-
-			var des = document.createElement('li');
-			var beerDes = document.createTextNode('Description : ' + data[i].description);
-			des.appendChild(beerDes);
-			des.style.fontSize = "small";
-
-			ul.appendChild(img);
-			ul.appendChild(name);
-			ul.appendChild(food);
-			ul.appendChild(des);
-			
-			div.appendChild(ul);
-
-			list.appendChild(div);
-		}
+	})
+	.then(function (data)
+	{
+		let result = "";
+        for (let i = 0; i < data.length; i++) {
+            result += '<div class="row" id="item">'
+						+ '<div class="col-sm-3">'
+							+ '<center>'
+								+ '<image src="' + data[i].image_url + '" id="img"/>'
+							+ '<center>'
+						+ '</div>'
+						+ '<div class="col-sm-9">'
+							+ '<b>Name : </b>' + data[i].name + '<br />'
+							+ '<b>Food Pairing : </b>' + data[i].food_pairing + '<br />'
+							+ '<b>Tagline : </b>' + data[i].tagline + '<br />'
+							+ '<b>Description : </b>' + data[i].description
+						+ '</div>'
+					+ '</div>';
+        }
+        document.getElementById('data').innerHTML = result
 	});
 }
